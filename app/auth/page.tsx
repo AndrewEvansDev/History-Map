28314e0b6f2b2ca7de9ea0c1bc209ede039554e7
Andrew Evans <78834502+AndrewEvansDev@users.noreply.github.com>
2024-04-06 00:37:06 -0700

    auth and hosting complete

---
+++ app/auth/page.tsx
@@ -0,0 +1,38 @@
+import { Card, CardDescription, CardHeader, CardTitle, CardContent} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { Github } from "lucide-react";
+import SigninWithGithub from "../components/SigninWithGithub";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../utils/auth";
+import { redirect } from "next/navigation";
+import SignInForm from "../components/SignInForm";
+
+
+export default async function AuthRoute(){
+    const session = await getServerSession(authOptions);
+
+if(session){
+    return redirect('/');
+}
+
+    return(
+        <div className="w-screen h-screen flex items-center justify-center">
+            <Card>
+            <CardHeader>
+                <CardTitle>Please sign in</CardTitle>
+                <CardDescription>Need to be auth'd to see page.</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div className="flex flex-col">
+                    <SignInForm />
+                    <SigninWithGithub />
+
+                </div>
+
+            </CardContent>
+        </Card>
+        </div>
+    );
+}
