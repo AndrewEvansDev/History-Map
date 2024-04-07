0bdbb3684196b1022edea6e1226a30452223ada3
Andrew Evans <78834502+AndrewEvansDev@users.noreply.github.com>
2024-04-07 05:30:11 -0700

    another attempt at masking, attempting to implement a rewrite instead of a redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
-// next.config.mjs
-
-const redirects = [
-{
-    source: '/(.*)',
-    destination: 'https://map.andrewevans.us/$1',
-    permanent: true,
-},
-];
-
-const nextConfig = {
-async redirects() {
-    return redirects;
-},
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
