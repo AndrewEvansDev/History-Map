8fe4fe9a1d07a7efa9d634b814486b7cdf13fd40
Andrew Evans <78834502+AndrewEvansDev@users.noreply.github.com>
2024-04-07 05:06:33 -0700

    setting up domain masking

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const redirects = [
+{
+    source: '/',
+    destination: 'https://map.andrewevans.us/',
+    permanent: true,
+},
+// Add more redirect rules as needed
+];
+
+const nextConfig = {
+async redirects() {
+    return redirects;
+},
+};
 
 export default nextConfig;
